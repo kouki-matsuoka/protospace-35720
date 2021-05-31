62c02214829a68c3ce08ef0f040294f8aae54da6
kouki-matsuoka <mdita05070507@gmail.com>
2021-05-31 18:48:44 +0900

    aaa

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+        has_many :prototypes
+        has_many :comments
+
+        validates :name, presence: true, length: { maximum: 6 }
 end
